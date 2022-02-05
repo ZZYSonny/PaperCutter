1bf056fc71e169aae50db7a1ffa7dc1353dfeee8
ZZYSonny <zhaozy5520@outlook.com>
2022-02-05 16:52:42 +0000

    refract

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from typing import Callable
 from numpy.typing import NDArray
 from pdf2image import convert_from_path
 from PyPDF2 import PdfFileWriter,PdfFileReader
+from PyPDF2.generic import RectangleObject
 import numpy as np
 import os
 import re
-
 tol=0.2
 
 def inRange(x:float,l:float,r:float) -> bool:
     return l<=x and x<=r
+
+def naturalSort(xs:list[str]) -> list[str]:
+    return sorted(
+        xs,
+        key=lambda s:int(re.sub('\D', '', s))
+    )
 
 class Border:
     def __init__(self, f:Callable[[int],NDArray]) -> None:
@@ -18,25 +24,31 @@ class Border:
         '''A function to read the i-th line'''
         self.scan=f
     
-    '''Advance the border line until the average color is in [l,r)'''
     def scanUntil(self,l,r) -> None:
+        '''Advance the border line until the average color is in [l,r)'''
         while(inRange(np.average(self.scan(self.cur)),l,r)): 
             self.cur+=1
     
-    '''Advance the border line, until a line with no content is met, and remains in this line'''
     def scanUntilEmpty(self) -> None:
+        '''Advance the border line, until a line with no content is met, and remains in this line'''
         self.scanUntil(0,255-tol)
     
-    '''Advance the border line, until a line with full content is met, and remains in this line'''
     def scanUntilContent(self) -> None:
+        '''Advance the border line, until a line with full content is met, and remains in this line'''
         self.scanUntil(255-tol,255)
 
 
 class CropPicture:
-    def __init__(self,image:np.ndarray) -> None:
+    def __init__(
+        self,
+        image:np.ndarray,
+        cropBox:RectangleObject,
+        attr: dict[str,bool]
+    ) -> None:
         self.image=image
-        height=int(self.image.shape[0])
-        width=int(self.image.shape[1])
+        self.attr=attr
+        (height,width)=self.image.shape
+        self.cropBox=cropBox
         self.TopBorder=Border(lambda i:image[i,:])
         self.BottomBorder=Border(lambda i:image[height-1-i,:])
         self.LeftBorder=Border(lambda i:image[:,i])
@@ -47,73 +59,111 @@ class CropPicture:
         self.LeftBorder.scanUntilContent()
         self.RightBorder.scanUntilContent()
 
-
-    '''Set the cropbox of PyPDF2 according to the four border line'''
-    def setCropBox(self,box):
+    def toCropbox(self) -> RectangleObject:
         height=int(self.image.shape[0])
         width=int(self.image.shape[1])
-        pdfWidth =int(box.getUpperRight()[0])
-        pdfHeight=int(box.getUpperRight()[1])
-        box.lowerLeft  = (
+        pdfWidth =int(self.cropBox.getUpperRight()[0])
+        pdfHeight=int(self.cropBox.getUpperRight()[1])
+        return RectangleObject([
             (self.LeftBorder.cur/width)*pdfWidth,
-            (1-(self.TopBorder.cur/height))*pdfHeight
-        )
-        box.upperRight = (
+            (1-(self.TopBorder.cur/height))*pdfHeight,
             (1-(self.RightBorder.cur/width))*pdfWidth,
             (self.BottomBorder.cur/height)*pdfHeight
-        )
+        ])
 
-'''Strategy for cropping'''
-def process(i:int, image: NDArray, page) -> list :
-    pic=CropPicture(image)
-    if i==0:
-        pic.BottomBorder.scanUntilEmpty()
-        pic.BottomBorder.scanUntilContent()
-    else:
-        pic.TopBorder.scanUntilEmpty()
-        pic.TopBorder.scanUntilContent()
-    pic.setCropBox(page.cropBox)
-    return [page]
 
-'''RuleCrop the file, and save to PdfFileWriter'''
-def work(inFilePath:str, outFile:PdfFileWriter):
-    images = convert_from_path(inFilePath,grayscale=True)
-    inFile = PdfFileReader(open(inFilePath,"rb"))
+def work(
+    images:np.ndarray, 
+    inFile:PdfFileReader, 
+    outFile:PdfFileWriter, 
+    process
+)->None:
+    '''RuleCrop the file, and save to PdfFileWriter'''
+    bookmarkL1 = [
+        inFile.getDestinationPageNumber(b)
+        for b in inFile.getOutlines()
+        if '/Page' in b
+    ]
     for i in range(len(images)):
-        for p in process(i,np.array(images[i]),inFile.getPage(i)):
-            outFile.addPage(p)
+        pic=CropPicture(
+            image=np.array(images[i]),
+            cropBox=inFile.getPage(i).cropBox,
+            attr={
+                'isFirst': i==0,
+                'isBookmark': i in bookmarkL1
+            }
+        )
+        for box in process(pic):
+            newp=inFile.getPage(i)
+            newp.cropBox=box.toCropbox()
+            outFile.addPage(newp)
 
-'''RuleCrop the file'''
-def workFile(inFilePath:str, outFilePath:str):
+def workFile(inFilePath:str, outFilePath:str, process):
+    '''- Crop the file
+       - Copy bookmarks
+    '''
+    #Load input PDF
+    inFile = PdfFileReader(open(inFilePath,"rb"))
     outFile= PdfFileWriter()
-    work(inFilePath,outFile)
-    with open(outFilePath,'wb') as outStream: outFile.write(outStream) 
+    
+    #Crop for each page
+    images = convert_from_path(inFilePath, grayscale=True)
+    work(images, inFile,outFile, process)
+    #outFile.addBookmarkDestination(inFile.getOutlines()[0])
+    #Save file to path
+    with open(outFilePath,'wb') as outStream: 
+        outFile.write(outStream) 
 
-'''RuleCrop the file in every folder'''
-def workFolder(inFolderPath:str, outFolderPath:str):
+def workFolder(inFolderPath:str, outFolderPath:str, process):
+    '''RuleCrop the file in every folder'''
     for inFileName in os.listdir(inFolderPath):
-        workFile(inFolderPath+"/"+inFileName, outFolderPath+"/"+inFileName)
+        inFilePath=inFolderPath+"/"+inFileName
+        outFilePath=outFolderPath+"/"+inFileName
+        workFile(inFilePath, outFilePath, process)
 
-'''For all pdfs in the folder, in the order of the integer they contain,
-- RuleCrop
-- Merge into one pdf
-- Add a bookmark
-'''
-def workFolderAndMerge(inFolderPath:str, outFilePath:str):
+def workFolderAndMerge(inFolderPath:str, outFilePath:str, process):
+    '''For all pdfs in the folder, in the order of the integer they contain,
+    - RuleCrop
+    - Merge into one pdf
+    - Add a bookmark
+    '''
     outFile= PdfFileWriter()
-    pdfList=sorted(
-        os.listdir(inFolderPath),
-        key=lambda s:int(re.sub('\D', '', s))
-    )
-    for fileName in pdfList:
+    for fileName in naturalSort(os.listdir(inFolderPath)):
         print("Start: "+fileName)
         nPageBefore=outFile.getNumPages()
-        work(inFolderPath+"/"+fileName,outFile)
+        work(inFolderPath+"/"+fileName,outFile, process)
         pdfName=fileName.split("/")[-1].split(".")[0]
         outFile.addBookmark(pdfName,nPageBefore , parent=None)
     print("Saving")
     with open(outFilePath,'wb') as outStream: outFile.write(outStream) 
 
+def processNothing(pic: CropPicture) -> list:
+    return [pic]
+
+def processQNote(pic: CropPicture) -> list :
+    if pic.attr['isFirst']:
+        pic.BottomBorder.scanUntilEmpty()
+        pic.BottomBorder.scanUntilContent()
+    else:
+        pic.TopBorder.scanUntilEmpty()
+        pic.TopBorder.scanUntilContent()
+    return [pic]
+
+def processAKNote(pic: CropPicture) -> list:
+    if pic.attr['isBookmark']:
+        pic.BottomBorder.scanUntilEmpty()
+        pic.BottomBorder.scanUntilContent()
+    else:
+        pic.TopBorder.scanUntilEmpty()
+        pic.TopBorder.scanUntilContent()
+    return [pic]
+
+def processArxiv(pic: CropPicture) -> list :
+    if pic.attr['isFirst']:
+        pic.LeftBorder.scanUntilEmpty()
+        pic.LeftBorder.scanUntilContent()
+    return [pic]
+
 #workFile("./in/1.pdf","./out/1.pdf")
-workFolder("./in","./out/")
-workFolderAndMerge("./in","./out/1.pdf")
+workFolder("./in","./out", processQNote)
+#workFolderAndMerge("./in","./out/1.pdf")
