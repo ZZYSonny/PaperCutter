e8d35d0a472e74cafa143a605ea3b23f7c2ebcf0
ZZYSonny <zhaozy5520@outlook.com>
2022-02-08 00:27:04 +0000

    rewrite with pymupdf

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import shutil
 from typing import Callable
 from numpy.typing import NDArray
-from pdf2image import convert_from_path
-from PyPDF2 import PdfFileWriter,PdfFileReader
-from PyPDF2.generic import RectangleObject
+import fitz
+from PIL import Image
+import numpy as np
 import numpy as np
 import os
 import re
@@ -41,154 +42,114 @@ class Border:
 class CropPicture:
     def __init__(
         self,
-        image:np.ndarray,
-        cropBox:RectangleObject,
+        page: fitz.Page,
         attr: dict[str,bool]
     ) -> None:
-        self.image=image
+        self.page=page
+        pix = page.getPixmap(colorspace='GRAY', alpha=False)
+        img = Image.frombytes('L', [pix.width, pix.height], pix.samples)
+        self.image=np.array(img)
         self.attr=attr
         (height,width)=self.image.shape
-        self.cropBox=cropBox
-        self.TopBorder=Border(lambda i:image[i,:])
-        self.BottomBorder=Border(lambda i:image[height-1-i,:])
-        self.LeftBorder=Border(lambda i:image[:,i])
-        self.RightBorder=Border(lambda i:image[:,width-1-i])
+        self.TopBorder=Border(lambda i:self.image[i,:])
+        self.BottomBorder=Border(lambda i:self.image[height-1-i,:])
+        self.LeftBorder=Border(lambda i:self.image[:,i])
+        self.RightBorder=Border(lambda i:self.image[:,width-1-i])
         '''Clean the white margin'''
         self.TopBorder.scanUntilContent()
         self.BottomBorder.scanUntilContent()
         self.LeftBorder.scanUntilContent()
         self.RightBorder.scanUntilContent()
 
-    def toCropbox(self) -> RectangleObject:
-        height=int(self.image.shape[0])
-        width=int(self.image.shape[1])
-        pdfWidth =int(self.cropBox.getUpperRight()[0])
-        pdfHeight=int(self.cropBox.getUpperRight()[1])
-        return RectangleObject([
+    def setCropbox(self) -> None:
+        height,width=self.image.shape
+        _,_,pdfWidth,pdfHeight=self.page.rect
+        self.page.set_cropbox(fitz.Rect([
             (self.LeftBorder.cur/width)*pdfWidth,
-            (1-(self.TopBorder.cur/height))*pdfHeight,
+            (self.TopBorder.cur/height)*pdfHeight,
             (1-(self.RightBorder.cur/width))*pdfWidth,
-            (self.BottomBorder.cur/height)*pdfHeight
-        ])
+            (1-(self.BottomBorder.cur/height))*pdfHeight
+        ]))
 
-
-def work(
-    images:np.ndarray, 
-    inFile:PdfFileReader, 
-    outFile:PdfFileWriter, 
-    process
-)->None:
-    '''RuleCrop the file, and save to PdfFileWriter'''
-    bookmarkL1 = [
-        inFile.getDestinationPageNumber(b)
-        for b in inFile.getOutlines()
-        if '/Page' in b
+def workFile(inPDF:fitz.Document, cropFunction):
+    '''- Read `inFilePath`
+       - Crop each page according to `process`
+       - Copy bookmarks
+       - Save to `outFilePath`
+    '''
+    bookmarkL1=[
+        page-1 for lvl,title,page in inPDF.get_toc()
+        if lvl==1
     ]
-    for i in range(len(images)):
-        pic=CropPicture(
-            image=np.array(images[i]),
-            cropBox=inFile.getPage(i).cropBox,
-            attr={
-                'isFirst': i==0,
-                'isBookmark': i in bookmarkL1
-            }
-        )
-        for box in process(pic):
-            newp=inFile.getPage(i)
-            newp.cropBox=box.toCropbox()
-            outFile.addPage(newp)
+    for i,page in enumerate(inPDF):
+        attr={
+            'isFirst': i==0,
+            'isBookmark': i in bookmarkL1
+        }
+        crop=CropPicture(page,attr)
+        if cropFunction is not None: cropFunction(crop)
+        crop.setCropbox()
 
-def copyBookmarks(outlines, inFile: PdfFileReader, outFile: PdfFileWriter, parent=None, offset=0):
-    '''Recursively add PyPDF2 bookmark to outFile'''
-    last=None
-    for b in outlines:
-        if '/Title' in b:
-            last=outFile.addBookmark(
-                title=b.title, 
-                pagenum=inFile.getDestinationPageNumber(b)+offset,
-                parent=parent
-            )
-        else:
-            copyBookmarks(b, inFile, outFile, last, offset)
-
-def workFile(inFilePath:str, outFilePath:str, process):
+def cropFile(inFilePath:str, outFilePath: str, cropFunction):
     '''- Read `inFilePath`
        - Crop each page according to `process`
        - Copy bookmarks
        - Save to `outFilePath`
     '''
     #Load input PDF
-    inFile = PdfFileReader(open(inFilePath,"rb"))
-    outFile= PdfFileWriter()
-    
-    #Crop for each page
-    images = convert_from_path(inFilePath, grayscale=True)
-    work(images, inFile,outFile, process)
-    
-    copyBookmarks(inFile.getOutlines(), inFile, outFile)
+    inPDF = fitz.open(inFilePath)
+    workFile(inPDF, cropFunction)
+    inPDF.save(outFilePath)
 
-    #Save file to path
-    with open(outFilePath,'wb') as outStream: 
-        outFile.write(outStream) 
-
-def workFolder(inFolderPath:str, outFolderPath:str, process):
+def cropFolder(inFolderPath:str, outFolderPath:str, cropFunction):
     '''- For every file in `inFolderPath`
        - `workFile` and save in a pdf of same name in `outFolderPath`    
     '''
     for inFileName in os.listdir(inFolderPath):
         inFilePath=inFolderPath+"/"+inFileName
         outFilePath=outFolderPath+"/"+inFileName
-        workFile(inFilePath, outFilePath, process)
+        cropFile(inFilePath, outFilePath, cropFunction)
 
-def workFolderAndMerge(inFolderPath:str, outFilePath:str, process):
-    '''- For every file in `inFolderPath`, in a natural ordering
-       - Crop each page according to `process`
-       - Add a bookmark as entry for every file
-    '''
-    outFile= PdfFileWriter()
+def mergeFolder(inFolderPath:str, outFilePath:str):
+    outPDF=fitz.Document()
+    outToC = []
     for fileName in naturalSort(os.listdir(inFolderPath)):
-        print("Start: "+fileName)
-        nPageBefore=outFile.getNumPages()
-        #Read file
-        inFilePath=inFolderPath+"/"+fileName
-        inFile = PdfFileReader(open(inFilePath,"rb"))
-        #Crop
-        images = convert_from_path(inFilePath, grayscale=True)
-        work(images, inFile, outFile, process)
-        #Add bookmark for file, and add sub bookmarks
-        entryName=fileName.split("/")[-1].split(".")[0]
-        entryForFile=outFile.addBookmark(entryName,nPageBefore)
-        copyBookmarks(inFile.getOutlines(), inFile, outFile, entryForFile, nPageBefore)
-    print("Saving")
-    with open(outFilePath,'wb') as outStream: outFile.write(outStream) 
+        nPageBefore = outPDF.page_count
+        inPDF = fitz.open(inFolderPath+"/"+fileName)
+        outPDF.insert_pdf(inPDF)
+        inToC = inPDF.get_toc(simple = False)
+        #Add an entry for the file
+        outToC.append([1, fileName, nPageBefore + 1])
+        #Add bookmark in inPDF under the bookmark for file
+        for item in inToC:
+            item[0]+=1
+            item[2]+=nPageBefore
+            item[3]['page']+=nPageBefore
+            outToC.append(item)
+    outPDF.set_toc(outToC)
+    outPDF.save(outFilePath)
 
-def processNothing(pic: CropPicture) -> list:
-    return [pic]
+def cropThenMerge(inFolderPath:str, outFilePath:str, cropFunction, tempDir:str):
+    if os.path.exists(tempDir): shutil.rmtree(tempDir)
+    os.mkdir(tempDir)
+    cropFolder(inFolderPath, tempDir, cropFunction)
+    mergeFolder(tempDir, outFilePath)
 
-def processQNote(pic: CropPicture) -> list :
+def cropQNote(pic: CropPicture):
     if pic.attr['isFirst']:
         pic.BottomBorder.scanUntilEmpty()
         pic.BottomBorder.scanUntilContent()
     else:
         pic.TopBorder.scanUntilEmpty()
         pic.TopBorder.scanUntilContent()
-    return [pic]
 
-def processAKNote(pic: CropPicture) -> list:
+def cropAKNote(pic: CropPicture):
     if pic.attr['isBookmark']:
         pic.BottomBorder.scanUntilEmpty()
         pic.BottomBorder.scanUntilContent()
     else:
         pic.TopBorder.scanUntilEmpty()
         pic.TopBorder.scanUntilContent()
-    return [pic]
 
-def processArxiv(pic: CropPicture) -> list :
-    if pic.attr['isFirst']:
-        pic.LeftBorder.scanUntilEmpty()
-        pic.LeftBorder.scanUntilContent()
-    return [pic]
-
-#workFile("./in/1.pdf","./out/1.pdf")
-#workFolder("./in","./out", processAKNote)
-workFolderAndMerge("./in","./out/1.pdf", processQNote)
+#workFile('./in/chapter.pdf', './out/chapter.pdf', cropQNote)
+cropThenMerge('./in','./out/merged.pdf', cropAKNote, './temp')
